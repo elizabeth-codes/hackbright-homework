41963ce8f591bdd348ec4bfcfb9d5dc31e39e2a8
Elizabeth Joyce <ekj28@cornell.edu>
2023-02-27 22:19:26 -0500

    create a report about customer payments

---
+++ melon-delivery-report/produce_summary.py
@@ -0,0 +1,53 @@
+def print_activity_log(file_name):
+    the_file = open(file_name)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+for i in range(1,4):
+    print(i)
+    print(f"Day {i}")
+    print_activity_log(f"um-deliveries-day-{i}.txt")
+
+# print("Day 2")
+# print_activity_log("um-deliveries-day-2.txt")
+
+# print("Day 3")
+# print_activity_log("um-deliveries-day-3.txt")
+
+
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
